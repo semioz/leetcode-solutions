3cecdf17a4f1a05ae67192d73778ef3bcff87b67
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-06-23 22:36:20 +0300

    part 1 completed

---
+++ Part 1/list_check.py
@@ -0,0 +1,5 @@
+#Write a function which accepts a list and returns True if each value in the list is a list. Otherwise the function should return False.
+
+def list_check(example):
+    return all(type(i) is list for i in example)
+
--- Part 2/includes.py
+++ Part 2/includes.py
@@ -1,3 +1,1 @@
-if msg.startswith("$translate"):
-    text = msg.split("$translate ",1)[1]
-    lang = msg.split("$translate ",2)[2]
+
