e4258045a4b1c949d19e06b9a3f763d04f6d5479
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-06-26 17:48:16 +0300

    Update list_check.py

--- The Modern Bootcamp/Part 1/list_check.py
+++ The Modern Bootcamp/Part 1/list_check.py
@@ -1,4 +1,4 @@
-#Wrie a function which accepts a list and returns True if each value in the list is a list. Otherwise the function should return False.
+#Write a function which accepts a list and returns True if each value in the list is a list. Otherwise the function should return False.
 
 def list_check(example):
     return all(type(i) is list for i in example)
