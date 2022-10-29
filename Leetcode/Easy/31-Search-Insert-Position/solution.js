e528cc439977bbfc8abf7a0886ee9061c0649621
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-10-29 14:08:43 +0300

    add search insert position

---
+++ Leetcode/Easy/31-Search-Insert-Position/solution.js
@@ -0,0 +1,11 @@
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.round((left + right) / 2);
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] > target) right = mid - 1;
+        else left = mid + 1;
+    }
+    return left;
+}
