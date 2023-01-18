00e327889e37d9969988081fb96e8b725e4edd5a
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-01-18 15:10:24 +0300

    add merge-two-linked-lists

---
+++ Leetcode/Easy/44-Merge-two-sorted-lists/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+let mergeTwoLists = function(l1, l2) {
+    let dummy = tail = new ListNode();
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            tail.next = l1;
+            l1 = l1.next;
+        } else {
+            tail.next = l2;
+            l2 = l2.next;
+        }
+        tail = tail.next;
+    }
+    if (l1) tail.next = l1;
+    if (l2) tail.next = l2;
+    return dummy.next;
+};
