caaab0cc572ca4252d095696912c9c9e4e15e4c5
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-02-20 22:22:00 +0300

    add same-tree

---
+++ Leetcode/Easy/Same-Tree/question.md
@@ -0,0 +1,6 @@
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
---
+++ Leetcode/Easy/Same-Tree/solution.py
@@ -0,0 +1,8 @@
+#Recursive Solution
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right) 
