45607f3f2eda6f3a5998ffce25903104d3848fa1
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-04-15 21:09:34 +0300

    add some py solution

---
+++ Leetcode/Easy/Fibonacci-Number/solution.py
@@ -0,0 +1,10 @@
+def fibo(n):
+    cache = {}
+    def fast(x):
+        if x in cache:
+            return cache[x]
+        if x < 2:
+            return x
+        cache[x] = fast(x-1)+fast(x-2)
+        return cache[x]
+    return fast(n)
