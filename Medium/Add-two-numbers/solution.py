79d58553e0d19c71e1b9a22fafc1f59988808b8e
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-07-03 12:38:02 +0300

    add twonumbers - go

--- Medium/Add-two-numbers/solution.py
+++ Medium/Add-two-numbers/solution.py
@@ -4,12 +4,28 @@
 #         self.val = val
 #         self.next = next
 
-# Brute Force - With helper functions
+# Initial Approach - With helper functions
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         finale = getFullNum(reverse(l1)) + getFullNum(reverse(l2))
         return reverse(numToLinkedList(finale))
 
+def reverse(head):
+    prev,curr = None,head
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    return prev
+
+def getFullNum(head):
+    fullnum = ""
+    curr = head
+    while curr:
+        fullnum += str(curr.val)
+        curr = curr.next
+    return int(fullnum)
 
 def numToLinkedList(number):
     number_str = str(number)
@@ -22,19 +38,26 @@ def numToLinkedList(number):
         curr = new_node
     return head
 
-def getFullNum(head):
-    fullnum = ""
-    curr = head
-    while curr:
-        fullnum += str(curr.val)
-        curr = curr.next
-    return int(fullnum)
 
-def reverse(head):
-    prev, curr = None,head
-    while curr:
-        temp = curr.next
-        curr.next = prev
-        prev = curr
-        curr = temp
-    return prev
+# ------------------------Second Approach------------------------
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            #new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            #update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
